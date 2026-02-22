8a7679fe7997fbdfb0675973a5780c3b12a4f354
Badea Ionut-Mirel <tupan.g@yahoo.com>
2026-02-22 23:26:01 +0100

    Fix settings with http_timeout_seconds

--- app/settings.py
+++ app/settings.py
@@ -1,9 +1,15 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
+
 class Settings(BaseSettings):
     app_name: str = "Weather Dashboard API"
-    cache_ttl_seconds: int = 600  # 10 minutes
+    cache_ttl_seconds: int = 600
+    http_timeout_seconds: int = 15  # timeout pentru requesturile HTTP
 
-    model_config = SettingsConfigDict(env_file=".env", extra="ignore")
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        extra="ignore"
+    )
+
 
 settings = Settings()
